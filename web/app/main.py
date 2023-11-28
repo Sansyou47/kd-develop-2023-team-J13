cdd1e7d8afe3c4a2f5c2f603e1acdd1499379f9c
YAMADA Manato <kd1334992@st.kobedenshi.ac.jp>
2023-11-28 14:55:25 +0900

    ストーリーの追加処置1

--- web/app/main.py
+++ web/app/main.py
@@ -6,3 +6,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return 'Test'
+
+@app.route('/action/create_stories',methods=['POST'])
+def add_stories():
+    stories = request.form.get('stories')
+    # MySQLへ接続
+    conn=mysql.get_db()
+    cur=conn.cursor()
+    # SQL実行
+    cur.execute("INSERT INTO employee(project_int,stories_name,stories) VALUES(%s,%s,%s)",(hoge,stories,hoge))
+    conn.commit()
+    cur.close()
+    return render_template('/templates/stories/create_stories.html')
