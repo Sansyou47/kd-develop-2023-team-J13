c0d3755e6a1871e55dbc81c4f7d70e6a6115cb59
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-12-04 10:51:36 +0900

    プロジェクト一覧をデータベースから取得する

--- web/app/main.py
+++ web/app/main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect
 from flaskext.mysql import MySQL
 from function import test
+import os
 
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_HOST'] = 'mysql'
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE')
+
+mysql=MySQL(app)
 
 app.register_blueprint(test.app)
 
@@ -29,6 +37,15 @@ def add_stories():
     conn.commit()
     cur.close()
     return render_template('/templates/stories/create_stories.html')
+
+@app.route('/select_project')
+def select_project():
+    # MySQLへ接続
+    cur = mysql.get_db().cursor()
+    # SQL実行
+    cur.execute("SELECT * FROM project")
+    data = cur.fetchall()
+    return render_template('/select_project.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
