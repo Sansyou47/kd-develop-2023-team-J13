ec92cafa531380893bd5033b0817a0d53bdd17dd
HASHIMOTO Syunpei <saltfish2229aks@gmail.com>
2023-12-05 09:42:45 +0900

    add(mainに追加とtaskの骨組み)

--- web/app/main.py
+++ web/app/main.py
@@ -6,34 +6,46 @@ import os
 app = Flask(__name__)
 
 # MySQL設定(環境変数から読み取り)
-app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_USER')
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
-app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE')
-app.config['MYSQL_DATABASE_HOST'] = 'mysql'
+app.config["MYSQL_DATABASE_USER"] = os.getenv("MYSQL_USER")
+app.config["MYSQL_DATABASE_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+app.config["MYSQL_DATABASE_DB"] = os.getenv("MYSQL_DATABASE")
+app.config["MYSQL_DATABASE_HOST"] = "mysql"
 
 mysql = MySQL(app)
 
 app.register_blueprint(test.app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return 'Test'
+    return "Test"
 
-@app.route('/create_storeis')
+
+@app.route("/create_storeis")
 def storeis():
-    return render_template('/templates/stories/create_stories.html')
+    return render_template("/templates/stories/create_stories.html")
 
-@app.route('/action/create_stories',methods=['POST'])
+
+@app.route("/task_task")
+def task():
+    return render_template("/task.html")
+
+
+@app.route("/action/create_stories", methods=["POST"])
 def add_stories():
-    stories = request.form.get('stories')
+    stories = request.form.get("stories")
     # MySQLへ接続
-    conn=mysql.get_db()
-    cur=conn.cursor()
+    conn = mysql.get_db()
+    cur = conn.cursor()
     # SQL実行
-    cur.execute("INSERT INTO employee(project_int,stories_name,stories) VALUES(%s,%s,%s)",(hoge,stories,hoge))
+    cur.execute(
+        "INSERT INTO employee(project_int,stories_name,stories) VALUES(%s,%s,%s)",
+        (hoge, stories, hoge),
+    )
     conn.commit()
     cur.close()
-    return render_template('/templates/stories/create_stories.html')
+    return render_template("/templates/stories/create_stories.html")
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
