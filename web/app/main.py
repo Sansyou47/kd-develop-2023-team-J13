38fa7ab9db5588223d1b5b0712291e878d1808ac
HASHIMOTO Syunpei <saltfish2229aks@gmail.com>
2023-12-05 11:37:29 +0900

    add(pullの為一旦)

--- web/app/main.py
+++ web/app/main.py
@@ -21,30 +21,9 @@ def index():
     return "Test"
 
 
-@app.route("/create_storeis")
-def storeis():
-    return render_template("/templates/stories/create_stories.html")
-
-
 @app.route("/task_task")
 def task():
     return render_template("/task.html")
-
-
-@app.route("/action/create_stories", methods=["POST"])
-def add_stories():
-    stories = request.form.get("stories")
-    # MySQLへ接続
-    conn = mysql.get_db()
-    cur = conn.cursor()
-    # SQL実行
-    cur.execute(
-        "INSERT INTO employee(project_int,stories_name,stories) VALUES(%s,%s,%s)",
-        (hoge, stories, hoge),
-    )
-    conn.commit()
-    cur.close()
-    return render_template("/templates/stories/create_stories.html")
 
 
 if __name__ == "__main__":
