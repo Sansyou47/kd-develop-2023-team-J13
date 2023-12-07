eed8d2bfab40fd423c485b282d70d97086a909e5
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-12-08 00:01:25 +0900

    ログイン関係

--- web/app/main.py
+++ web/app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, session, url_for
 from flaskext.mysql import MySQL
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 from flask import jsonify
 from function import story, project, task, init_session, apple
 import os
@@ -26,12 +27,54 @@ task.mysql = mysql
 init_session.mysql = mysql
 apple.mysql = mysql
 
-app.secret_key = "your_secret_key"
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-@app.route("/")
+class User(UserMixin):
+    def __init__(self, user_id):
+        self.id = user_id
+
+@login_manager.user_loader
+def load_user(user_id):
+    cursor = mysql.get_db().cursor()
+    cursor.execute('SELECT * FROM users WHERE userId = %s', (user_id))
+    user = cursor.fetchone()
+    if user:
+        return User(user_id)
+    else:
+        return None
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        userid = request.form['userid']
+        password = request.form['password']
+        cursor = mysql.get_db().cursor()
+        cursor.execute('SELECT * FROM users WHERE userId = %s', (userid))
+        user = cursor.fetchone()
+        if user and check_password_hash(user['password'], password):
+            login_user(User(user['id']))
+            return redirect(url_for('index'))
+        else:
+            return 'Invalid username or password'
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/')
+@login_required
 def index():
-    data = str(session.get("project"))
-    return data
+    return 'Logged in as: ' + current_user.id
+
+# @app.route("/")
+# def index():
+#     data = str(session.get("project"))
+#     return data
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
