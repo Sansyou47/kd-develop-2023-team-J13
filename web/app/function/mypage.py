e4e124409fc9e50081cf6b97b1e6eb0ca9917e9a
HASHIMOTO Syunpei <saltfish2229aks@gmail.com>
2023-12-13 13:58:16 +0900

    add(今度こそ完成)

--- web/app/function/mypage.py
+++ web/app/function/mypage.py
@@ -5,12 +5,30 @@ mypage = Blueprint("mypage", __name__)
 
 mysql = None
 
+userNumber = 4
+
+@mypage.route("/register_skill", methods=["POST"])
+def register_skill():
+    # MySQLへ接続
+    conn = mysql.get_db()
+    cur = conn.cursor()
+
+    # フォームから送信された各スキルの値を取得し、データベースに格納
+    for skill_index in range(1, 7):
+        skill_value = request.form.get(f"skill{skill_index}")
+        cur.execute(f"UPDATE skill SET skill{skill_index} = %s WHERE userNumber = %s", (skill_value, userNumber))
+
+    conn.commit()
+    cur.close()
+
+    return redirect(url_for("mypage.add_mypage"))
+
 @mypage.route("/mypage")
 def add_mypage():
     # MySQLへ接続
     conn = mysql.get_db()
     cur = conn.cursor()
-    userNumber = 4
+
     # userNumberがデータベースに存在するか確認
     cur.execute("SELECT * FROM skill WHERE userNumber = %s", (userNumber,))
     result = cur.fetchone()
@@ -23,8 +41,11 @@ def add_mypage():
     # SQL実行
     cur.execute("SELECT * FROM skill")
     skills = cur.fetchall()
+    # userNumberから名前を取り出す
+    cur.execute("SELECT users.userName FROM users JOIN skill ON users.userNumber = skill.userNumber WHERE users.userNumber = %s", (userNumber,))
+    name = cur.fetchone()
 
     conn.commit()
     cur.close()
 
-    return render_template("/mypage/mypage.html", skills=skills, userNumber=userNumber)
+    return render_template("/mypage/mypage.html", skills=skills, userNumber=userNumber, name=name)
