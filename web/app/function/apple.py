8a1a1e788b59501eccb32db32ad0596e8d7e9193
YONEZU Daiya <kd1329246@st.kobedenshi.ac.jp>
2023-12-14 13:44:03 +0900

    a

--- web/app/function/apple.py
+++ web/app/function/apple.py
@@ -15,25 +15,27 @@ def create_project1():
 
 @apple.route('/action/create_project1', methods=['POST'])
 def action_create_project1():
-
     conn = mysql.get_db()
     cur = conn.cursor()
+
     if request.method == 'POST':
-        # POSTメソッドでの処理
+        # プロジェクト情報を取得
         projectTitle = request.form.get('projectTitle')
         startDate = request.form.get('startDate')
         endDate = request.form.get('endDate')
         collaborator = request.form.get('collaborator')
         urlInput = request.form.get('urlInput')
         sharedFolderInput = request.form.get('sharedFolderInput')
-        #projectの追加が必要
-        cur.execute("INSERT INTO project(name,start_date,update_date,owner,github,googleDrive) VALUES(%s,%s,%s,%s,%s,%s)", (projectTitle,startDate,endDate,collaborator,urlInput,sharedFolderInput))
+
+        # プロジェクト情報を保存
+        cur.execute("INSERT INTO project(name, start_date, update_date, owner, github, googleDrive) VALUES (%s, %s, %s, %s, %s, %s)", (projectTitle, startDate, endDate, collaborator, urlInput, sharedFolderInput))
         conn.commit()
+
     cur.close()
     conn.close()
 
-
     return redirect('/select_project')
+
 
 
 @apple.route('/create_project2')
