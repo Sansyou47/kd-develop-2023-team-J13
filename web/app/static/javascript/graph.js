97adb34b1d541671c8692ae06f565a3f290e0dd2
NAKAI ZEN <kd1293049@st.kobedenshi.ac.jp>
2023-12-14 09:33:32 +0900

    graph

--- web/app/static/javascript/graph.js
+++ web/app/static/javascript/graph.js
@@ -1,23 +1,35 @@
+//HTML要素の取得
 var container = document.getElementById("visualization"),
-    items = new vis.DataSet();
-var groups = [];
-var te = [];
-
+    //データセットの初期化
+    items = new vis.DataSet(),
+    groups = new vis.DataSet(),
+    task = [];
+//storyGraphの各要素に対して処理
 for (var i = 0; i < storyGraph.length; i++) {
-    te.push({ id: i, group: storyGraph[i][3], content: storyGraph[i][1], start: '2022-12-05', end: '2023-12-25' });
-    groups.push({ id: i, content: storyGraph[i][3] });
+    //taskに必要な情報を格納
+    task.push({ id: i, group: storyGraph[i][3], content: storyGraph[i][0], start: storyGraph[i][5], end: storyGraph[i][6] });
+    //これ例"プロジェクト選択画面",2,null,"ファイルを一括で管理したい",1,"Tue, 12 Dec 2023 11:59:50 GMT","Tue, 12 Dec 2023 11:59:50 GMT"
+    // groupsに既に同じcontent(例:プロジェクト選択画面)があるかを確認
+    var existingGroups = groups.get({
+        filter: function (item) {
+            return item.content === storyGraph[i][3];
+        }
+    });
+
+    // なければ追加
+    if (existingGroups.length === 0) {
+        groups.add({ id: storyGraph[i][3], content: storyGraph[i][3] });
+    }
 }
 
-for (var j = 0; j < te.length; j++) {
-    console.log(te[j]);
-    items.add(te[j]);
+//itemsにtaskの値を渡す
+for (var j = 0; j < task.length; j++) {
+    items.add(task[j]);
 }
-
-
 
 var options = {
-    start: '2023-12-06',  // timeline軸が表す期間の範囲の開始日
-    end: '2023-12-15',
+    start: '2023-12-01',  // timeline軸が表す期間の範囲の開始日
+    end: '2023-12-30',
     editable: true,
     //zoomの有無
     zoomable: true,
@@ -32,7 +44,7 @@ var options = {
 };
 //timelineに表示する項目を書く
 //groupsを書かないと一番左のCC,DD等が表示されなくなる
-var timeline = new vis.Timeline(container, items, options, groups);
+var timeline = new vis.Timeline(container, items, groups, options);
 
 timeline.addCustomTime('2014-02-20', 'v-bar');
 
