2122faba615ede9cdb0724197d1e8b512b57a803
YAMADA Manato <kd1334992@st.kobedenshi.ac.jp>
2023-12-15 13:15:50 +0900

    画面遷移のやつ

--- web/app/function/graph.py
+++ web/app/function/graph.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, session
 from flask_login import login_required
 from flaskext.mysql import MySQL
 import json
@@ -10,6 +10,7 @@ mysql = None
 @graph.route("/graph")
 @login_required
 def graphs():
+    project = str(session.get("project"))
     # MySQLへ接続
     conn = mysql.get_db()
     cur = conn.cursor()
@@ -20,4 +21,4 @@ def graphs():
     conn.commit()
     cur.close()
     # story_graphを取得するコード
-    return render_template('graph.html',story_graph = story_graph)
+    return render_template('graph.html',story_graph = story_graph,project=project)
