4e2b3f002f877a7b38e71744dcbf2c6d89d50984
Nobuhiro Hayashi <kd1334632@st.kobedenshi.ac.jp>
2023-12-18 10:15:00 +0900

    taskNameの取得

--- web/app/function/delete.py
+++ web/app/function/delete.py
@@ -17,6 +17,7 @@ def delete_data():
     projectName = cur.fetchall()
     #空のタプル配列の宣言
     storyName = ()
+    taskName = ()
     #プロジェクトネームの要素数分だけ繰り返す
     for i in range(len(projectName)):
         cur.execute("SELECT name FROM story WHERE project = %s", (projectName[i]))
@@ -24,7 +25,14 @@ def delete_data():
         #区切りの為に0を入れる
         storyName += ('0',) #タプル宣言の為にコンマを入れる
     
+    for i in range(len(storyName)):
+        if storyName[i] == '0':
+            taskName += ('0',)
+        else :
+            cur.execute("SELECT name FROM task WHERE story = %s", (storyName[i]))
+            taskName += cur.fetchall()
+    
 
     conn.commit()
     cur.close()
-    return render_template("/delete/delete_data.html", projectName = projectName, storyName = storyName)
+    return render_template("/delete/delete_data.html", projectName = projectName, storyName = storyName, taskName = taskName)
